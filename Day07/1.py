197408473149d5f55134313105f72020aed42052
brukidm <mislavbrusac0@gmail.com>
2021-01-09 14:04:56 +0100

    removed useless arg

--- Day07/1.py
+++ Day07/1.py
@@ -2,19 +2,19 @@ import re
 
 bags_list = set()
 
-def search_for_bags(rules, bag, total):
+def search_for_bags(rules, bag):
     regex = r"[1-9]* " + bag + r" bag"
     for i in range(len(rules)):
         if re.search(regex, rules[i]):
             new_bag = rules[i].split(" ")[0] + " " + rules[i].split(" ")[1]
             bags_list.add(new_bag)
-            search_for_bags(rules, new_bag, total)
+            search_for_bags(rules, new_bag)
         else:
             continue
-    return total
+    return
 
 
 with open(r"input") as f:
     lines = f.read().split("\n")
-    total = search_for_bags(lines, "shiny gold", 0)
+    search_for_bags(lines, "shiny gold")
     print(len(bags_list))
